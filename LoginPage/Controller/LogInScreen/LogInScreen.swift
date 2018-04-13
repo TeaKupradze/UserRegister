b9f71c778c72c8f04557515ad9174f94e7cf4481
tea kupradze <teakoo555@gmail.com>
2018-04-13 19:01:42 +0400

    add new logIn Screen

---
+++ LoginPage/Controller/LogInScreen/LogInScreen.swift
@@ -0,0 +1,19 @@
+//
+//  LogInScreen.swift
+//  LoginPage
+//
+//  Created by Tea Kupradze on 4/13/18.
+//  Copyright © 2018 Sergey Nikolaev. All rights reserved.
+//
+
+import UIKit
+
+class LogInScreen: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    
+    }
+
+}
